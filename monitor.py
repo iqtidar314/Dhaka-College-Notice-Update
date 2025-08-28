a53db02a97650b5a95d272b3f744072d01f40fd8
iqtidar314 <iqtidar3.14@gmail.com>
2025-08-28 09:56:54 +0600

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -299,11 +299,8 @@ class NoticeMonitor:
         try:
             notices_content = []
             for notice in notices:
-                content = f"{notice.get('title', '')}{notice.get('date', '')}{notice.get('download_url', '')}"
-                #for testing cache changes
-                if notice>5:
-                    content = f"{notice.get('title', '')}{notice.get('download_url', '')}"
-                notices_content.append(content)
+                content = f"{notice.get('title', '')}{notice.get('download_url', '')}"
+
             
             notices_content.sort()
             combined_content = "".join(notices_content)
